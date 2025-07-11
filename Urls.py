3f49fe5e71071440d647556c6be508ee1316c1f8
Deepak-454 <gupta.deepak54207@gmail.com>
2025-07-11 19:45:50 +0530

    Create Urls.py

---
+++ Urls.py
@@ -0,0 +1,6 @@
+urlpatterns=[path('receipes/',receipes,name="reciepes"),
+    path('delete_receipe/<id>/',delete_receipe,name="delete_receipes"),
+    path('update_receipe/<id>/',update_receipe,name="update_receipe"),
+    path('login/',login_page,name="login"),
+    path('logout/',logout_page,name="logout"),
+    path('Register/',register,name="register")]
